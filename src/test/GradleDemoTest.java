85453653249c8555d025f829d81df9719f50a843
Jennifer Bramson <jib41@pitt.edu>
2016-09-27 14:14:31 -0400

    Added semicolons

--- src/test/GradleDemoTest.java
+++ src/test/GradleDemoTest.java
@@ -7,37 +7,37 @@ public class GradleDemoTest {
 
   @Test
   public void testTri1() {
-    assertEquals(GradleDemo.Tri(1), 1)
+    assertEquals(GradleDemo.Tri(1), 1);
   }
 
   @Test
   public void testTri2() {
-    assertEquals(GradleDemo.Tri(2), 3)
+    assertEquals(GradleDemo.Tri(2), 3);
   }
 
   @Test
   public void testTri3() {
-    assertEquals(GradleDemo.Tri(3), 6)
+    assertEquals(GradleDemo.Tri(3), 6);
   }
 
   @Test
   public void testLazy1() {
-    assertEquals(GradleDemo.Lazy(0), 1)
+    assertEquals(GradleDemo.Lazy(0), 1);
   }
 
   @Test
   public void testLazy2() {
-    assertEquals(GradleDemo.Lazy(1), 2)
+    assertEquals(GradleDemo.Lazy(1), 2);
   }
 
   @Test
   public void testLazy3() {
-    assertEquals(GradleDemo.Lazy(2), 4)
+    assertEquals(GradleDemo.Lazy(2), 4);
   }
 
   @Test
   public void testLazy4() {
-    assertEquals(GradleDemo.Lazy(3), 7)
+    assertEquals(GradleDemo.Lazy(3), 7);
   }
 
 }
