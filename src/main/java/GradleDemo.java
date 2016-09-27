2b871ac145214c273d1eca2563d8152268bf97d1
Cory Trbojevic <cmt75@pitt.edu>
2016-09-27 13:34:19 -0400

    functions are working

---
+++ src/main/java/GradleDemo.java
@@ -0,0 +1,30 @@
+public class GradleDemo{
+
+  public static void main(String[] args){
+    String arg1 = args[0];
+    int arg2 = 0;
+
+    try{
+      arg2 = Integer.valueOf(args[1]);
+    }
+    catch(Exception e){
+      System.out.println("That was not an int.");
+      System.exit(0);
+    }
+
+    if(arg1.equals("lazy")){
+      Lazy(arg2);
+    }
+    else if(arg1.equals("triangle")){
+      Tri(arg2);
+    }
+  }
+
+  public static void Tri(int n){
+    System.out.println("Tri(" + n + ") = " + n*(n+1)/2);
+  }
+
+  public static void Lazy(int n){
+    System.out.println("Lazy(" + n + ") = " + (Math.pow(n, 2) + n + 2)/2);
+  }
+}
